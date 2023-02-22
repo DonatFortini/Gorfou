a87405febb081a667342c3d5933d8373859246ae
Donat Fortini <donatfortini2a@gmail.com>
2023-02-22 15:36:42 +0100

    first push

---
+++ app.py
@@ -0,0 +1,71 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
+
+from grid1 import build_grid as build_grid1
+from grid2 import build_grid as build_grid2
+from grid3 import build_grid as build_grid3
+
+
+class Navbar(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.size_hint_y = 0.1
+        self.add_widget(Button(text="Importer des données"))
+
+
+class MenuButton(Button):
+    def __init__(self, text, grid_builder, **kwargs):
+        super().__init__(**kwargs)
+        self.text = text
+        self.grid_builder = grid_builder
+
+
+class MainScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        
+        menu_buttons = [
+            MenuButton("Grid 1", build_grid1),
+            MenuButton("Grid 2", build_grid2),
+            MenuButton("Grid 3", build_grid3)
+        ]
+
+        
+        menu_layout = GridLayout(cols=1, size_hint_x=0.2)
+        for button in menu_buttons:
+            button.bind(on_press=self.show_grid)
+            menu_layout.add_widget(button)
+
+        
+        self.add_widget(menu_layout)
+        self.current_grid = build_grid1()
+        self.add_widget(self.current_grid)
+
+        
+    def show_grid(self, button):
+        
+        self.remove_widget(self.current_grid)
+        self.current_grid = button.grid_builder()
+        self.add_widget(self.current_grid)
+
+
+class MyApp(App):
+    def build(self):
+        Window.size = (1400, 700)
+        Window.top = 100
+        Window.left = 100
+
+        
+        main_layout = BoxLayout(orientation="vertical")
+        main_layout.add_widget(Navbar())
+        main_layout.add_widget(MainScreen())
+
+        return main_layout
+
+
+if __name__ == "__main__":
+    MyApp().run()
---
+++ grid2.py
@@ -0,0 +1,27 @@
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+def build_grid():
+    grid_layout = GridLayout(cols=3, padding=10, spacing=10)
+
+    button1 = Button(text="Button 2", on_press=lambda _: print("Button 1 clicked"))
+    button2 = Button(text="Button 2", on_press=lambda _: print("Button 3 clicked"))
+    button3 = Button(text="Button 2", on_press=lambda _: print("Button 3 clicked"))
+    button4 = Button(text="Button 2", on_press=lambda _: print("Button 4 clicked"))
+    button5 = Button(text="Button 2", on_press=lambda _: print("Button 5 clicked"))
+    button6 = Button(text="Button 2", on_press=lambda _: print("Button 6 clicked"))
+    button7 = Button(text="Button 2", on_press=lambda _: print("Button 7 clicked"))
+    button8 = Button(text="Button 2", on_press=lambda _: print("Button 8 clicked"))
+    button9 = Button(text="Button 2", on_press=lambda _: print("Button 9 clicked"))
+
+    grid_layout.add_widget(button1)
+    grid_layout.add_widget(button2)
+    grid_layout.add_widget(button3)
+    grid_layout.add_widget(button4)
+    grid_layout.add_widget(button5)
+    grid_layout.add_widget(button6)
+    grid_layout.add_widget(button7)
+    grid_layout.add_widget(button8)
+    grid_layout.add_widget(button9)
+
+    return grid_layout
