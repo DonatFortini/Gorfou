b5424fdecca1f48d6f38bcd471ed6bd6f7e27111
kappablanka <theo.piacentini@gmail.com>
2023-03-09 17:16:30 +0100

    refactoring fichier resources

--- src/gorfou_app/title_screen.py
+++ src/gorfou_app/title_screen.py
@@ -4,30 +4,53 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 
+
 class TitleScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.name = 'title'
+        self.name = "title"
 
         with self.canvas.before:
             Color(0.24, 0.23, 0.23)
             self.rect = Rectangle(size=self.size, pos=self.pos)
             self.bind(size=self._update_rect, pos=self._update_rect)
 
-        self.title = Label(text='Gorfou-IA', font_size='50sp', pos_hint={'center_x': 0.5, 'center_y': 0.9})
-        self.image = Image(source='image/Gorfou.png', pos_hint={'center_x': 0.5, 'center_y': 0.6},
-                           size_hint=(0.6, 0.6))
-        
+        self.title = Label(
+            text="Gorfou-IA",
+            font_size="50sp",
+            pos_hint={"center_x": 0.5, "center_y": 0.9},
+        )
+        self.image = Image(
+            source="../resources/image/Gorfou.png",
+            pos_hint={"center_x": 0.5, "center_y": 0.6},
+            size_hint=(0.6, 0.6),
+        )
 
-        self.import_button = Button(text='Importer\n     des\ndonnées', font_size='20sp',
-                                    size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.4},
-                                    on_press=self.play)
-        self.traitement = Button(text='Traitement\n     de\ndonnées', font_size='20sp', size_hint=(0.2, 0.1),
-                              pos_hint={'center_x': 0.25, 'center_y': 0.25})
-        self.model = Button(text='Création\n     de\nmodèle', font_size='20sp', size_hint=(0.2, 0.1),
-                              pos_hint={'center_x': 0.5, 'center_y': 0.25})
-        self.visual = Button(text='Visualisation', font_size='20sp', size_hint=(0.2, 0.1),
-                              pos_hint={'center_x': 0.75, 'center_y': 0.25})
+        self.import_button = Button(
+            text="Importer\n     des\ndonnées",
+            font_size="20sp",
+            size_hint=(0.2, 0.1),
+            pos_hint={"center_x": 0.5, "center_y": 0.4},
+            on_press=self.play,
+        )
+        self.traitement = Button(
+            text="Traitement\n     de\ndonnées",
+            font_size="20sp",
+            size_hint=(0.2, 0.1),
+            pos_hint={"center_x": 0.25, "center_y": 0.25},
+        )
+        self.model = Button(
+            text="Création\n     de\nmodèle",
+            font_size="20sp",
+            size_hint=(0.2, 0.1),
+            pos_hint={"center_x": 0.5, "center_y": 0.25},
+        )
+        self.visual = Button(
+            text="Visualisation",
+            font_size="20sp",
+            size_hint=(0.2, 0.1),
+            pos_hint={"center_x": 0.75, "center_y": 0.25},
+        )
 
         self.add_widget(self.title)
         self.add_widget(self.image)
@@ -42,4 +65,4 @@ class TitleScreen(Screen):
 
     def play(self, instance):
         screen_manager = self.manager
-        screen_manager.current = 'main'
+        screen_manager.current = "main"
