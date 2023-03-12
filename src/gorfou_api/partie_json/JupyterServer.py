1b61fc49d37e9c142fd8f1d37785a07965765c36
kappablanka <theo.piacentini@gmail.com>
2023-03-12 14:57:54 +0100

    passage à l'OOP pour le serveur jupyter

---
+++ src/gorfou_api/partie_json/JupyterServer.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import subprocess
+import re
+import time
+import logging
+module_logger = logging.getLogger(__name__)
+
+
+class JupyterServer:
+    POWERSHELL = r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+
+    def __init__(self, port=8099, shell=POWERSHELL) -> None:
+        self.port = port
+        self.shell
+        self.process = None
+
+    def stop_server(self):
+        subprocess.run(
+            [self.shell, 'jupyter', 'notebook', 'stop', self.port], stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+
+        self.process.kill()
+
+        print("serveur bien fermé !")
+
+    def run_server(self):
+        self.process = subprocess.Popen(
+            [self.shell, 'jupyter', 'notebook', '--no-browser', '--port=8099'], stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+        print("t1")
+
+    def get_token(self) -> str:
+        server_launch = subprocess.run(
+            [self.shell, "jupyter", "notebook", "list"], capture_output=True)
+
+        module_logger.info(
+            f"return of powershell command {server_launch.stdout}")
+
+        command_result = server_launch.stdout.decode("ascii")
+        token_search = re.search("\?token=[\w]+", command_result)
+        if token_search == None:
+            raise LookupError("token not found !")
+        module_logger.info("token found !")
+
+        return token_search.group(0)
+
+    def __str__(self) -> str:
+        pass
