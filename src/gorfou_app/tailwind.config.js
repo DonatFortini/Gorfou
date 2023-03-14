ccbf19e83d4bbadf4cce14759cb391a74bc8e2c4
kappablanka <theo.piacentini@gmail.com>
2023-03-14 23:13:53 +0100

    debut passage à tailwind

--- src/gorfou_app/tailwind.config.js
+++ src/gorfou_app/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@catppuccin/tailwindcss")],
 };
