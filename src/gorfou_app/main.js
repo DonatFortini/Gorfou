1c7f73fc9d45e9e5af233760311abdd434e9751d
kappablanka <theo.piacentini@gmail.com>
2023-03-15 09:35:48 +0100

    reverse fichiers supprimé

--- src/gorfou_app/main.js
+++ src/gorfou_app/main.js
@@ -1,22 +1,22 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1400,
     height: 700,
     webPreferences: {
-      contentSecurityPolicy:
-        "default-src 'self'; style-src 'self' https://example.com",
+      contentSecurityPolicy: "default-src 'self'; style-src 'self' https://example.com"
     },
-  });
+  })
 
-  win.loadFile("index.html");
-};
+  win.loadFile('index.html')
+}
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
-  app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
-  });
-});
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+})
+
