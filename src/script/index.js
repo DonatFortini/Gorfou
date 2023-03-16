12840c15568b768e063867ba568f19e333c82215
kappablanka <theo.piacentini@gmail.com>
2023-03-16 11:31:21 +0100

    début implémentation drag and drop

--- src/script/index.js
+++ src/script/index.js
@@ -1,21 +1,29 @@
-
-const button_import = document.getElementById('import');
-button_import.addEventListener('click', () => {
-  remote.showOpenDialog({
-    properties: ['openDirectory'],
-    title: 'Select Folder'
-  }).then(result => {
-    console.log(result.filePaths);
-  }).catch(err => {
-    console.log(err);
-  });
+const button_import = document.getElementById("import");
+button_import.addEventListener("click", () => {
+  remote
+    .showOpenDialog({
+      properties: ["openDirectory"],
+      title: "Select Folder",
+    })
+    .then((result) => {
+      console.log(result.filePaths);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-const button_transfo = document.getElementById('transf');
-button_transfo.addEventListener('click',function(){window.location.assign("main.html")});
+const button_transfo = document.getElementById("transf");
+button_transfo.addEventListener("click", function () {
+  window.location.assign("test_drag_and_drop.html");
+});
 
-const button_creatio = document.getElementById('creati');
-button_creatio.addEventListener('click',function(){window.location.assign("main.html")});
+const button_creatio = document.getElementById("creati");
+button_creatio.addEventListener("click", function () {
+  window.location.assign("main.html");
+});
 
-const button_visual = document.getElementById('visual');
-button_visual.addEventListener('click',function(){window.location.assign("main.html")});
+const button_visual = document.getElementById("visual");
+button_visual.addEventListener("click", function () {
+  window.location.assign("main.html");
+});
