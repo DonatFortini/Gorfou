d8e02d75cdb003589d1c25a96342763427bfd363
kappablanka <theo.piacentini@gmail.com>
2023-03-16 11:34:12 +0100

    ajout alt img

--- src/ts/test_python.ts
+++ src/ts/test_python.ts
@@ -1,13 +1,11 @@
 import { PythonShell } from "python-shell";
 
-var python_shell_1 = require("python-shell");
-var options = {
+let options = {
   mode: "text",
   pythonOptions: ["-u"],
 };
-python_shell_1.PythonShell.run("src/gorfou_api/", options).then(function (
-  messages
-) {
+
+PythonShell.run("src/gorfou_api/").then(function (messages) {
   // results is an array consisting of messages collected during execution
   console.log("results: %j", messages);
 });
